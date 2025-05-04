458cd4a73a5f5e6dddce9673d1319cd6d35f35a0
winterzzz0 <abdalrhmanmicro1@gmail.com>
2025-05-04 07:39:35 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello f rom EC 122!2"
+    return "Hello f ro m EC 122!2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
